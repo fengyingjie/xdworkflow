8fd4a8a6c33cf64587bf831d3995936f052f5d32
fengyingjie <feng_yingjie1982@163.com>
2020-05-28 23:43:18 +0800

    dragelist

--- src/app/new-question/new-question.component.ts
+++ src/app/new-question/new-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   templateUrl: './new-question.component.html',
@@ -8,9 +9,37 @@ export class NewQuestionComponent implements OnInit {
 
   shouldRun = 'ee';//[/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 
+  movies = [
+    {'type':'text1'},
+    {'type':'text2'},
+    {'type':'text3'},
+    {'type':'text4'},
+    {'type':'text5'},
+    {'type':'text6'},
+    {'type':'text7'},
+    {'type':'text1'},
+    {'type':'text2'},
+    {'type':'text3'},
+    {'type':'text4'},
+    {'type':'text5'},
+    {'type':'text6'},
+    {'type':'text7'},
+    {'type':'text1'},
+    {'type':'text2'},
+    {'type':'text3'},
+    {'type':'text4'},
+    {'type':'text5'},
+    {'type':'text6'},
+    {'type':'text7'},
+    {'type':'text8'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  drop(event: CdkDragDrop<{}[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
 }
